bfef9114a47812e8837598a6eccb5a2ba8c8babe
Greg Beard <greg.beard@vodafone.com>
2018-02-15 17:49:14 +0000

    Concrete imp for `and_then`

--- src/and_then.rs
+++ src/and_then.rs
@@ -1,9 +1,12 @@
+use std::mem;
+
 use pollable::Pollable;
 use result::PollResult;
 
 pub enum AndThen<L, F, R> {
     First(L, F),
     Second(R),
+    Done,
 }
 
 impl<L, F, R> AndThen<L, F, R> where
@@ -25,6 +28,30 @@ impl<L, F, R> Pollable for AndThen<L, F, R> where
     type Error = R::Error;
 
     fn poll(&mut self) -> Result<PollResult<Self::Item>, Self::Error> {
+        let result = match *self {
+            AndThen::First(ref mut left, _) => match left.poll() {
+                Ok(PollResult::Ready(value)) => Ok(value),
+                Ok(PollResult::NotReady) => return Ok(PollResult::NotReady),
+                Err(e) => Err(e),
+            },
+            AndThen::Second(ref mut right) => return right.poll(),
+            AndThen::Done => panic!("Poll called on finished result"),
+        };
+        
+        let next = match mem::replace(self, AndThen::Done) {
+            AndThen::First(_, f) => {
+                let left_value = result?;
+                let mut right = f(left_value);
+                if let PollResult::Ready(right_value) = right.poll()? {
+                    return Ok(PollResult::Ready(right_value));
+                }
+
+                AndThen::Second(right)
+            },
+            _ => unreachable!(),
+        };
+
+        *self = next;
         Ok(PollResult::NotReady)
     }
 }
